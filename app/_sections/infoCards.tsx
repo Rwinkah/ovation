7ee830ed86fef46dd431bfca6adecc11da8df516
Pelumi Amao <113313095+Pekumiiix@users.noreply.github.com>
2024-06-10 06:42:39 +0100

    adjusted media query

--- app/_sections/infoCards.tsx
+++ app/_sections/infoCards.tsx
@@ -27,7 +27,7 @@ export default function InfoCards() {
   ]
 
   return (
-    <section className="w-[70%] grid grid-cols-2 gap-x-[20px] gap-y-12 py-20 more-section">
+    <section className="w-[70%] grid grid-cols-2 gap-x-5 gap-y-12 py-20 info-card">
       {moreCards.map((card, index) => (
         <div
           className="p-[10px] flex flex-col items-center rounded-[20px] bg-card-gradient border h-fit py-10 info-card"
