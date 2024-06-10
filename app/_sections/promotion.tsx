7d5c54b629d88d71c68d2fcbce5e587538ab9673
Pelumi Amao <113313095+Pekumiiix@users.noreply.github.com>
2024-06-10 06:09:22 +0100

    adjusted the ui a little

---
+++ app/_sections/promotion.tsx
@@ -0,0 +1,11 @@
+export default function Promotion() {
+  return (
+    <section className="w-full h-[100px] py-3 bg-[#C1FE17] flex items-center justify-center gap-4 promotion">
+      <p className="text-2xl text-[#0B0A10] font-semibold">
+        Ovation Secures $Archway $10,000 grant
+      </p>
+
+      <img className="h-8 w-8" src="assets/images/arrow-right.svg" alt="" />
+    </section>
+  )
+}
