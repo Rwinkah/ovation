f3442f31869fb89d3ccd611a0ec6004aea97941f
Pelumi Amao <113313095+Pekumiiix@users.noreply.github.com>
2024-06-13 06:27:21 +0100

    adjusted ui

--- app/_sections/infoCards.tsx
+++ app/_sections/infoCards.tsx
@@ -5,6 +5,7 @@ export default function InfoCards() {
       description:
         'Users will be enabled to have more meaningful interactions, the ability to create unique posts, recognise post contributions, and filtering.',
       imgSrc: 'assets/images/info/info1-3x.png',
+      smallSreenImage: 'assets/images/info/info1-sm.png',
       forSmallerScreens: false,
     },
     {
@@ -12,6 +13,7 @@ export default function InfoCards() {
       description:
         'Through blockchain interoperability, and an NFT centric UI/UX, users have one pane into their NFT portfolio, notoriety, and social notoriety.',
       imgSrc: 'assets/images/info/info2-3x.png',
+      smallSreenImage: 'assets/images/info/info2-sm.png',
       forSmallerScreens: false,
     },
     {
@@ -19,6 +21,7 @@ export default function InfoCards() {
       description:
         'Earn $OVA tokens in a variety of ways: Sharing NFTs, Earning badges, Drawing engagement, Interacting on the platform, Owning a Genesis NFT',
       imgSrc: 'assets/images/info/info3-3x.png',
+      smallSreenImage: 'assets/images/info/info3-sm.png',
       forSmallerScreens: false,
     },
     {
@@ -26,6 +29,7 @@ export default function InfoCards() {
       description:
         'Discover NFT collections by genre, ranking, trending, and other metrics. Users are recommended collections based upon their interests and NFT portfolio.',
       imgSrc: 'assets/images/info/info4-3x.png',
+      smallSreenImage: 'assets/images/info/info-4sm.png',
       forSmallerScreens: true,
     },
   ]
@@ -34,21 +38,33 @@ export default function InfoCards() {
     <section className="container w-[70%] grid grid-cols-2 gap-x-5 gap-y-12 py-20 info-card">
       {moreCards.map((card, index) => (
         <div
-          className="p-[10px] flex flex-col items-center rounded-[20px] bg-card-gradient border h-auto py-10 info-card"
+          className={`p-[10px] flex flex-col items-center rounded-[20px] bg-card-gradient border h-auto py-10 info-card ${card.forSmallerScreens ? 'pr-0' : ''}`}
           key={index}
         >
           <img
-            className="rounded-lg p-[8px] w-auto h-[295px]"
+            className="rounded-lg p-[8px] w-auto h-[295px] large-screen"
             src={card.imgSrc}
             alt=""
           />
 
+          <img
+            className={`rounded-lg p-[8px] hidden small-device ${card.forSmallerScreens ? 'ml-[12.75%] w-[90%] h-auto' : 'w-auto h-[295px]'}`}
+            src={card.smallSreenImage}
+            alt=""
+          />
+
           <div className="flex flex-col px-[6px] py-5 gap-3 mt-20">
-            <p className="text-white text-2xl font-medium text-center">
+            <p
+              className={`text-white text-2xl font-medium text-center ${card.forSmallerScreens ? 'pr-[10px]' : ''}`}
+            >
               {card.header}
             </p>
 
-            <p className="text-xl text-center">{card.description}</p>
+            <p
+              className={`text-xl text-center ${card.forSmallerScreens ? 'pr-[10px]' : ''}`}
+            >
+              {card.description}
+            </p>
           </div>
         </div>
       ))}
