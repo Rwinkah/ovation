61fe48dd99175527108c7d379b05cc3f5d68123b
emma-kingsley <159794365+emma-kingsley@users.noreply.github.com>
2024-07-06 03:04:44 +0100

    nav links disabled

--- app/_sections/nav.tsx
+++ app/_sections/nav.tsx
@@ -1,159 +1,162 @@
-'use client'
-import { useState } from 'react'
-import { HamburgerMenuIcon } from '@radix-ui/react-icons'
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export function Header({ navLinks = links }: HeaderProps) {
-  return (
-    <header className="relative border-b border-[#FFFFFF33] bg-transparent">
-      <nav className="container mx-auto  flex w-full items-center justify-between  px-4 md:px-6 py-6">
-        <a href="/">
-          <Logo />
-        </a>
-        <Navigation navLinks={navLinks} />
-        <Navigation mobile navLinks={navLinks} />
-      </nav>
-    </header>
-  )
+	return (
+		<header className="relative border-b border-[#FFFFFF33] bg-transparent">
+			<nav className="container mx-auto  flex w-full items-center justify-between  px-4 md:px-6 py-6">
+				<a href="/">
+					<Logo />
+				</a>
+				<Navigation navLinks={navLinks} />
+				<Navigation mobile navLinks={navLinks} />
+			</nav>
+		</header>
+	);
 }
 
 function Logo() {
-  return (
-    <Image
-      src="/assets/images/logo/logo.png"
-      width={200}
-      height={40}
-      alt="logo"
-      className="-ml-[15%] sm:ml-0"
-    />
-  )
+	return (
+		<Image
+			src="/assets/images/logo/logo.png"
+			width={200}
+			height={40}
+			alt="logo"
+			className="-ml-[15%] sm:ml-0"
+		/>
+	);
 }
 const links: NavLink[] = [
-  {
-    title: 'Learn',
-    href: '#',
-  },
-  {
-    title: 'News',
-    href: '#',
-  },
-  {
-    title: 'Founder NFT',
-    href: '#',
-  },
-]
+	{
+		title: "Learn",
+		href: "#",
+	},
+	{
+		title: "News",
+		href: "#",
+	},
+	{
+		title: "Founder NFT",
+		href: "#",
+	},
+];
 
 function Navigation({ mobile = false, navLinks = [] }: NavigationProps) {
-  const [mobileNavigationOpened, setMobileNavigationOpened] = useState(false)
+	const [mobileNavigationOpened, setMobileNavigationOpened] = useState(false);
 
-  const navClassName = ` text-lg text-primary-foreground space-x-2
+	const navClassName = ` text-lg text-primary-foreground space-x-2
     ${
-      mobile
-        ? `transition transform -right-full absolute top-[90px] z-20 py-4 pb-7 w-full overflow-y-auto sm:hidden backdrop-filter backdrop-blur-md ${
-            mobileNavigationOpened
-              ? '-translate-x-full visible bg-[#111310] opacity-100'
-              : 'hidden opacity-0'
-          }`
-        : 'hidden sm:flex'
-    }
-  `
+			mobile
+				? `transition transform -right-full absolute top-[90px] z-20 py-4 pb-7 w-full overflow-y-auto sm:hidden backdrop-filter backdrop-blur-md ${
+						mobileNavigationOpened
+							? "-translate-x-full visible bg-[#111310] opacity-100"
+							: "hidden opacity-0"
+					}`
+				: "hidden sm:flex"
+		}
+  `;
 
-  const navListClassName = `
+	const navListClassName = `
     flex
-    ${mobile ? 'flex-col space-y-2 w-full ' : 'items-center gap-5'}
-  `
-  const navListItemClassName = `
+    ${mobile ? "flex-col space-y-2 w-full " : "items-center gap-5"}
+  `;
+	const navListItemClassName = `
     group relative rounded-full  px-5 py-2 hover:shadow-[0px_4px_20px_0px_#AFC76B4D] text-lg text-primary-foreground transform transition-transform
-    ${mobile ? 'w-full overflow-x-visible' : ''}
-  `
-  const navListLinkClassName = mobile ? 'mx-2 rounded-[20px]' : ''
+    ${mobile ? "w-full overflow-x-visible" : ""}
+  `;
+	const navListLinkClassName = mobile ? "mx-2 rounded-[20px]" : "";
 
-  const closeMobileNavigation = () => setMobileNavigationOpened(false)
+	const closeMobileNavigation = () => setMobileNavigationOpened(false);
 
-  return (
-    <>
-      {mobile && (
-        <div className="flex gap-2 md:hidden ">
-          <Button
-            className="block bg-transparent sm:hidden"
-            onClick={() => setMobileNavigationOpened(true)}
-            title="Open navigation menu"
-          >
-            <HamburgerMenuIcon height={'15px'} width={'20px'} color="#CFF073" />
-          </Button>
-        </div>
-      )}
+	return (
+		<>
+			{mobile && (
+				<div className="flex gap-2 md:hidden ">
+					<Button
+						className="block bg-transparent sm:hidden"
+						onClick={() => setMobileNavigationOpened(true)}
+						title="Open navigation menu"
+					>
+						<HamburgerMenuIcon height={"15px"} width={"20px"} color="#CFF073" />
+					</Button>
+				</div>
+			)}
 
-      {mobile && mobileNavigationOpened && (
-        <Button
-          className="fixed right-0 top-0 z-10 h-full w-full opacity-0 sm:hidden"
-          onClick={closeMobileNavigation}
-        />
-      )}
+			{mobile && mobileNavigationOpened && (
+				<Button
+					className="fixed right-0 top-0 z-10 h-full w-full opacity-0 sm:hidden"
+					onClick={closeMobileNavigation}
+				/>
+			)}
 
-      <nav className={navClassName}>
-        <ul className={navListClassName}>
-          {navLinks.map(({ title, href, button }) => (
-            <li
-              className={navListItemClassName}
-              key={href}
-              onClick={closeMobileNavigation}
-              onKeyUp={() => {}}
-              onKeyDown={() => {}}
-            >
-              {button ? (
-                <Button variant={'default'} className={` ${mobile && 'mx-4'}`}>
-                  <a href={href}>{title}</a>
-                </Button>
-              ) : (
-                <NavLink
-                  className={navListLinkClassName}
-                  mobile={mobile}
-                  href={href}
-                >
-                  {title}
-                </NavLink>
-              )}
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </>
-  )
+			<nav className={navClassName}>
+				<ul className={navListClassName}>
+					{navLinks.map(({ title, href, button }) => (
+						<li
+							className={navListItemClassName}
+							key={href}
+							onClick={closeMobileNavigation}
+							onKeyUp={() => {}}
+							onKeyDown={() => {}}
+						>
+							{button ? (
+								<Button variant={"default"} className={` ${mobile && "mx-4"}`}>
+									<a href={href}>{title}</a>
+								</Button>
+							) : (
+								<NavLink
+									className={navListLinkClassName}
+									mobile={mobile}
+									href={href}
+								>
+									{title}
+								</NavLink>
+							)}
+						</li>
+					))}
+				</ul>
+			</nav>
+		</>
+	);
 }
 
 function NavLink({ children, className, mobile, href }: NavLinkProps) {
-  return (
-    <a
-      className={`
+	return (
+		<Button variant={"ghost"} disabled>
+			<a
+				className={`
         block whitespace-nowrap  text-lg text-primary-foreground no-underline transition 
-        ${mobile && ''}
+        ${mobile && ""}
         ${className}
-      `}
-      href={href}
-    >
-      {children}
-    </a>
-  )
+        `}
+				aria-disabled
+				href={href}
+			>
+				{children}
+			</a>
+		</Button>
+	);
 }
 
 export interface NavLink {
-  title: string
-  href: string
-  children?: NavLink[]
-  button?: boolean
+	title: string;
+	href: string;
+	children?: NavLink[];
+	button?: boolean;
 }
 
 interface NavigationProps {
-  mobile?: boolean
-  navLinks?: NavLink[]
+	mobile?: boolean;
+	navLinks?: NavLink[];
 }
 
 interface NavLinkProps extends React.HTMLProps<HTMLLinkElement> {
-  mobile?: boolean
+	mobile?: boolean;
 }
 
 interface HeaderProps {
-  navLinks?: NavLink[]
+	navLinks?: NavLink[];
 }
