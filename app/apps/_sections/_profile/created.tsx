f362ca8790f46ec4258af085872a672e4e8aa3a6
Pelumi Amao <113313095+Pekumiiix@users.noreply.github.com>
2024-07-25 19:02:38 +0100

    adjusted the created tab

--- app/apps/_sections/_profile/created.tsx
+++ app/apps/_sections/_profile/created.tsx
@@ -11,7 +11,7 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import LikeIcon from '@/components/icons/likeIcon'
-import { Item } from '@radix-ui/react-select'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 interface Button {
   name: string
@@ -97,79 +97,97 @@ export default function Created() {
           <p className="text-white font-medium">Created</p>
 
           <div className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              {buttons.map((item, index) => (
-                <Button
-                  variant="default"
-                  className={`${item.isActive ? 'bg-white text-[#232227]' : 'bg-[#232227] text-[#999999]'} p-3 rounded-[50px] h-fit text-xs`}
-                  key={index}
-                >
-                  {item.name + '(' + item.itemCount + ')'}
-                </Button>
-              ))}
-            </div>
-
-            <Select>
-              <SelectTrigger className="w-fit rounded-[7px] otline-none border border-[#232227] text-white text-xs bg-[#333333]">
-                <SelectValue placeholder="Filters" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-
-          <div className="flex flex-wrap gap-4">
-            {items.map((item, index) => (
-              <div
-                className="w-[296px] flex flex-col bg-[#18181C] border border-[#FFFFFF14] rounded-[10px]"
-                key={index}
-              >
-                <Image
-                  src={item.imgSrc}
-                  alt="NFT Preview"
-                  width={317}
-                  height={217}
-                  className="rounded-t-[10px]"
-                />
-
-                <div className="flex flex-col items-center justify-between bg-[#111115] border border-[#FFFFFF0D] px-3 py-3 rounded-b-[10px] gap-3">
-                  <div className="flex items-center justify-between w-full">
-                    <div className="flex flex-col items-start">
-                      <p className="text-[11px] text-[#999999]">Artist</p>
-                      <p className="text-white text-sm font-semibold">
-                        {item.artist}
-                      </p>
-                    </div>
-                    <div className="flex flex-col items-end">
-                      <p className="text-[11px] text-[#999999]">Price</p>
-                      <p className="text-white text-sm font-semibold">
-                        {item.price}
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="flex w-full items-center justify-between pt-3 border-t border-[#353538]">
-                    <div className="flex items-center gap-1">
-                      <LikeIcon className="w-4 h-4 stroke-white" />
-                      <p className="text-xs text-white font-normal">
-                        {item.likeCount}
-                      </p>
-                    </div>
-
-                    <Button
-                      variant="default"
-                      className={`${item.isHidden ? 'border border-[#CFF073] bg-transparent text-[#CFF073]' : 'bg-[#CFF073] text-[#111115]'} px-3 py-[6px] h-fit text-[10px] font-medium`}
-                      onClick={() => toggleHidden(index)}
+            <Tabs defaultValue="All" className="w-full">
+              <TabsList className="flex justify-between items-center">
+                <div className="flex items-center gap-2">
+                  {buttons.map((item, index) => (
+                    <TabsTrigger
+                      value={item.name}
+                      className="bg-[#232227] text-[#999999] p-3 rounded-[50px] h-fit text-xs border-none data-[state=active]:bg-white data-[state=active]:text-[#232227]"
+                      key={index}
                     >
-                      {item.isHidden ? 'Unhide' : 'Hide'}
-                    </Button>
-                  </div>
+                      {item.name + '(' + item.itemCount + ')'}
+                    </TabsTrigger>
+                  ))}
                 </div>
-              </div>
-            ))}
+
+                <Select>
+                  <SelectTrigger className="w-fit rounded-[7px] otline-none border border-[#232227] text-white text-xs bg-[#333333]">
+                    <SelectValue placeholder="Filters" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="light">Light</SelectItem>
+                    <SelectItem value="dark">Dark</SelectItem>
+                    <SelectItem value="system">System</SelectItem>
+                  </SelectContent>
+                </Select>
+              </TabsList>
+              <TabsContent value="All">
+                <div className="flex flex-wrap gap-4">
+                  {items.map((item, index) => (
+                    <div
+                      className="w-[296px] flex flex-col bg-[#18181C] border border-[#FFFFFF14] rounded-[10px]"
+                      key={index}
+                    >
+                      <Image
+                        src={item.imgSrc}
+                        alt="NFT Preview"
+                        width={317}
+                        height={217}
+                        className="rounded-t-[10px]"
+                      />
+
+                      <div className="flex flex-col items-center justify-between bg-[#111115] border border-[#FFFFFF0D] px-3 py-3 rounded-b-[10px] gap-3">
+                        <div className="flex items-center justify-between w-full">
+                          <div className="flex flex-col items-start">
+                            <p className="text-[11px] text-[#999999]">Artist</p>
+                            <p className="text-white text-sm font-semibold">
+                              {item.artist}
+                            </p>
+                          </div>
+                          <div className="flex flex-col items-end">
+                            <p className="text-[11px] text-[#999999]">Price</p>
+                            <p className="text-white text-sm font-semibold">
+                              {item.price}
+                            </p>
+                          </div>
+                        </div>
+
+                        <div className="flex w-full items-center justify-between pt-3 border-t border-[#353538]">
+                          <div className="flex items-center gap-1">
+                            <LikeIcon className="w-4 h-4 stroke-white" />
+                            <p className="text-xs text-white font-normal">
+                              {item.likeCount}
+                            </p>
+                          </div>
+
+                          <Button
+                            variant="default"
+                            className={`${item.isHidden ? 'border border-[#CFF073] bg-transparent text-[#CFF073]' : 'bg-[#CFF073] text-[#111115]'} px-3 py-[6px] h-fit text-[10px] font-medium`}
+                            onClick={() => toggleHidden(index)}
+                          >
+                            {item.isHidden ? 'Unhide' : 'Hide'}
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </TabsContent>
+              <TabsContent value="Complete">
+                Change your password here.
+              </TabsContent>
+              <TabsContent value="Domain">
+                Change your password here.
+              </TabsContent>
+              <TabsContent value="Collectibles">
+                Change your password here.
+              </TabsContent>
+              <TabsContent value="Metaverse">
+                Change your password here.
+              </TabsContent>
+              <TabsContent value="Art">Change your password here.</TabsContent>
+            </Tabs>
           </div>
         </div>
       </div>
