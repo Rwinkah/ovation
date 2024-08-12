f82a7396987f56a3fba594ea35b89899d47d0905
Pelumi Amao <113313095+Pekumiiix@users.noreply.github.com>
2024-08-12 15:16:18 +0100

    major ui change

---
+++ app/apps/_components/_notification/follower-new-exp-notification.tsx
@@ -0,0 +1,47 @@
+import { Button } from '@/components/ui/button'
+
+export default function FollowerExpNotification({
+  userProfilePicture,
+  notificationTypeImg,
+  userName,
+  className,
+}: any) {
+  return (
+    <div
+      className={`${className} w-full items-center justify-between px-5 py-4 md:py-7 md:px-8 border-b border-[#1A1A1A]`}
+    >
+      <div className="flex items-center gap-4 w-fit">
+        <div className="flex h-fit relative">
+          <img
+            src={userProfilePicture}
+            alt="User Display Picture"
+            className="w-[30px] h-[30px] md:w-[44px] md:h-[44px]"
+          />
+
+          <img
+            src={notificationTypeImg}
+            alt="User Display Picture"
+            width={16}
+            height={16}
+            className="absolute bottom-0 right-[-3px] w-2 h-2 md:w-4 md:h-4"
+          />
+        </div>
+        <div className="w-fit">
+          <p className="text-sm md:text-lg text-white font-medium w-fit">
+            New experience
+          </p>
+          <p className="text-[#B3B3B3] text-xs">
+            {userName + ' ' + '- just added a new experience'}
+          </p>
+        </div>
+      </div>
+
+      <Button
+        variant="default"
+        className={`bg-white text-[10px] text-[#0B0A10] px-2 py-[6px] border-none outline-none h-fit transition-all duration-300 hover:bg-[#B3B3B3]`}
+      >
+        Make a post
+      </Button>
+    </div>
+  )
+}
---
+++ app/apps/_components/_notification/personal-new-exp.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@/components/ui/button'
+
+export default function PersonalNewExpNotification({
+  userProfilePicture,
+  className,
+}: any) {
+  return (
+    <div
+      className={`${className} w-full items-center justify-between px-5 py-4 md:py-7 md:px-8 border-b border-[#1A1A1A]`}
+    >
+      <div className="flex items-center gap-4 w-fit">
+        <div className="flex items-center justify-center relative bg-[#232227] rounded-full h-10 w-10">
+          <img
+            src={userProfilePicture}
+            alt="User Display Picture"
+            className="w-[19px] h-[19px]"
+          />
+        </div>
+        <div className="w-fit">
+          <p className="text-sm md:text-lg text-white font-medium w-fit">
+            New Experience
+          </p>
+          <p className="text-[#B3B3B3] text-xs">
+            Congratulations on your new role
+          </p>
+        </div>
+      </div>
+
+      <Button
+        variant="default"
+        className={`bg-white text-[10px] text-[#0B0A10] px-2 py-[6px] border-none outline-none h-fit transition-all duration-300 hover:bg-[#B3B3B3]`}
+      >
+        Make a post
+      </Button>
+    </div>
+  )
+}
